29b8283b188de1bdb0e8c14fd09b1d6c5803915f
maksdor q <dorohin.maksim7474@mail.ru>
2024-09-27 00:26:45 +0200

    lab 5 done

--- lab5,var5/main.cpp
+++ lab5,var5/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <locale.h>
+#include <string>
 using namespace std;
 
 int main()
@@ -11,8 +12,8 @@ int main()
 	const int max = 5;
 	cout << "Напиши 5 чисел:" << endl;
 	float numbers[max];
-	float sum = 1;
-	int i{}, min = 0, p = 0;
+	float sum = 1, min = 0;
+	int i{}, p = 0;
 	while (i < max)
 	{
 		cout << i + 1 << "-й элемент: ";
@@ -29,15 +30,27 @@ int main()
 	cout << sum << endl;
 	for (i = 0; i < max; ++i)
 	{
-		if ((numbers[i] < 0 && (floor(numbers[i]) - numbers[i]) != 0) && numbers[i] < min)
+		if ((numbers[i] < 0 && (floor(numbers[i]) - numbers[i]) != 0) && (numbers[i] < min))
 			min = numbers[i], p = i;
 		else
 			continue;
 	}
-	cout << min << p << endl;
+	cout << min << " " << p+1 << endl;
+
 	//2
 
-	long long n;
+	int n;
 	cout << "n: ";
-
+	cin >> n;
+	int x{}, minx = 9, ln{};
+	string str_n = to_string(n);
+	while (x < str_n.length())
+	{
+		ln = n % 10;
+		if (ln < minx)
+			minx = ln;
+		n /= 10;
+		x++;
+	}
+	cout << ln;
 }
